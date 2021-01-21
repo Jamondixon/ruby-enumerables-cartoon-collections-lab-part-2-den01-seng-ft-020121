5f2831f45df055faaf30f0fd6a856e99d2b11e42
Jamon Dixon <jamonadixon@gmail.com>
2021-01-21 17:43:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,3 +28,4 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   { |planeteer_call| valid_calls.include?(planeteer_call) }
 end
+end
